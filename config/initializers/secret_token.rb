ebe71d1c49573b170ad4b8f421cf64f510b49496
Matt Buck <github@matthewbuck.com>
2013-10-16 03:24:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResponsivePriceTable::Application.config.secret_key_base = '258dcdfb007d775310668ead8d2263619e18af09350cf2e3fde127b5107a4bb28faad182e5d9b88bc5a9621c16741ebf7bb5a80c87967dbd13ebb59cce600403'
